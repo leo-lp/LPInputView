babbf5c47d4f9375f469d30b52a423045f842afe
leo-lp <lipengmjy@163.com>
2018-05-24 14:37:01 +0800

    Modify voice test.

--- LPInputViewDemo/LPInputViewDemo/Custom/LPInputAudioRecordIndicatorView.swift
+++ LPInputViewDemo/LPInputViewDemo/Custom/LPInputAudioRecordIndicatorView.swift
@@ -10,7 +10,8 @@ import UIKit
 import LPInputView
 
 class LPInputAudioRecordIndicatorView: UIView {
-
+    let stateLbl: UILabel = UILabel(frame: UIScreen.main.bounds)
+    
     deinit {
         print("LPInputAudioRecordIndicatorView: -> release memory.")
     }
@@ -31,23 +32,29 @@ class LPInputAudioRecordIndicatorView: UIView {
     func setup() {
         backgroundColor = UIColor(white: 0, alpha: 0.65)
         
+        stateLbl.textColor = UIColor.white
+        stateLbl.textAlignment = .center
+        stateLbl.font = UIFont.boldSystemFont(ofSize: 17)
+        addSubview(stateLbl)
     }
 }
 
 extension LPInputAudioRecordIndicatorView: LPRecordButtonDelegate {
     
     func recordButton(_ recordButton: LPRecordButton, recordPhase: LPAudioRecordPhase) {
+        let msg: String
         switch recordPhase {
         case .start:
-            print("开始录制...")
+            msg = "开始录制..."
         case .recording:
-            print("录制中... “手指上滑，取消发送”")
+            msg = "录制中... “手指上滑，取消发送”"
         case .finished:
-            print("录制完成...")
+            msg = "录制完成..."
         case .cancelling:
-            print("正在取消录制中... “松开手指，取消发送”")
+            msg = "正在取消录制中... “松开手指，取消发送”"
         case .cancelled:
-            print("已经取消录制...")
+            msg = "已经取消录制..."
         }
+        stateLbl.text = msg
     }
 }
